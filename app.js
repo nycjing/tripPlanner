2193ab088e6a3bdfba44d9dedb23b52f1d6b3ecd
Jing Jia <nycjing@gmail.com>
2017-08-11 16:55:28 -0400

    routing

--- app.js
+++ app.js
@@ -18,13 +18,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 const path = require('path');
 app.use(express.static(path.join(__dirname, '/public')));
-
+app.use(express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.use(express.static(path.join(__dirname, '/node_modules/bootstrap/dist')));
 app.use('/trip',route);
 
 app.get('/', function (req, res, next) {
 
   res.render('home');
 });
+
 
 
 app.use(function (err, req, res, next) {
